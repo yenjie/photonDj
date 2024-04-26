73dc53e258623a4db3bb2492fdf528462fe82c67
Yen-Jie Lee <yenjie@mit.edu>
2024-04-26 09:50:50 -0400

    update the code to store the photon and jets in an event-by-event basis

--- plot.C
+++ plot.C
@@ -1,240 +1,146 @@
 #include "JetTree.C"
 #include <cmath>
 #include <iostream>
+#include <vector>
 
-class Data {
+class EventData {
 public:
-  Double_t photonPt, photonEta, photonPhi, jetPt, jetEta, jetPhi, dphi, dj, weight;
+    std::vector<Double_t> photonPts;
+    std::vector<Double_t> photonEtas;
+    std::vector<Double_t> photonPhis;
+    std::vector<Double_t> jetPts;
+    std::vector<Double_t> jetEtas;
+    std::vector<Double_t> jetPhis;
+    Double_t weight;
 };
 
+void setBranches(TTree *tree, EventData &data) {
+    tree->Branch("photonPt", &data.photonPts);
+    tree->Branch("photonEta", &data.photonEtas);
+    tree->Branch("photonPhi", &data.photonPhis);
+    tree->Branch("jetPt", &data.jetPts);
+    tree->Branch("jetEta", &data.jetEtas);
+    tree->Branch("jetPhi", &data.jetPhis);
+    tree->Branch("weight", &data.weight, "weight/D");
+}
 
-void setBranch(TTree *outputTree, Data *data)
-{
-  outputTree->Branch("photonPt", &data->photonPt, "photonPt/D");
-  outputTree->Branch("photonEta", &data->photonEta, "photonEta/D");
-  outputTree->Branch("photonPhi", &data->photonPhi, "photonPhi/D");
-  outputTree->Branch("jetPt", &data->jetPt, "jetPt/D");
-  outputTree->Branch("jetEta", &data->jetEta, "jetEta/D");
-  outputTree->Branch("jetPhi", &data->jetPhi, "jetPhi/D");
-  outputTree->Branch("dphi", &data->dphi, "dphi/D");
-  outputTree->Branch("dj", &data->dj, "dj/D");
-  outputTree->Branch("weight", &data->weight, "weight/D");
+double calcPhi(double phi) {
+    phi = fmod(phi + M_PI, 2 * M_PI);
+    if (phi < 0) {
+        phi += 2 * M_PI;
+    }
+    phi -= M_PI;
+    return phi;
+}
+
+void processEvent(JetTree *j, TH1D* h, EventData &data, TTree *tree) {
+    h->Sumw2();
+
+    double sumEntry = 0;
+    for (long long int i = 0; i < j->fChain->GetEntries(); i++) {
+        if (i % 10000 == 0) std::cout << i << " " << j->fChain->GetEntries() << std::endl;
+	data.photonPts.clear();
+        data.photonEtas.clear();
+        data.photonPhis.clear();
+        data.jetPts.clear();
+        data.jetEtas.clear();
+        data.jetPhis.clear();
+        j->GetEntry(i);
+        if ((*j->LeadingPhotonPt)[0] < 40) continue;
+
+        bool hasValidJets = false;
+        std::vector<Double_t> eventJetPts;
+        std::vector<Double_t> eventJetEtas;
+        std::vector<Double_t> eventJetPhis;
+
+        // Loop over jets in the event
+        for (size_t k = 0; k < j->SignalJet03JewelPt->size(); k++) {
+            if ((*j->SignalJet03JewelPt)[k] > 30 && fabs((*j->SignalJet03JewelEta)[k]) < 1.6) {
+                double dPhi_jgamma = fabs(calcPhi((*j->SignalJet03JewelPhi)[k] - (*j->LeadingPhotonPhi)[0]));
+                if (dPhi_jgamma < 3.14159 / 2.) continue;
+
+                eventJetPts.push_back((*j->SignalJet03JewelPt)[k]);
+                eventJetEtas.push_back((*j->SignalJet03JewelEta)[k]);
+                eventJetPhis.push_back((*j->SignalJet03JewelPhi)[k]);
+                hasValidJets = true;
+            }
+        }
+
+        if (hasValidJets) {
+            data.photonPts.push_back((*j->LeadingPhotonPt)[0]);
+            data.photonEtas.push_back((*j->LeadingPhotonEta)[0]);
+            data.photonPhis.push_back((*j->LeadingPhotonPhi)[0]);
+            data.jetPts.insert(data.jetPts.end(), eventJetPts.begin(), eventJetPts.end());
+            data.jetEtas.insert(data.jetEtas.end(), eventJetEtas.begin(), eventJetEtas.end());
+            data.jetPhis.insert(data.jetPhis.end(), eventJetPhis.begin(), eventJetPhis.end());
+
+            data.weight = (*j->EventWeight)[0];
+            sumEntry += data.weight;
+            tree->Fill();
+        }
+    }
+
+    h->Scale(1. / sumEntry);
+    for (int i = 0; i < h->GetNbinsX(); i++) {
+        h->SetBinContent(i, h->GetBinContent(i) / h->GetBinWidth(i));
+        h->SetBinError(i, h->GetBinError(i) / h->GetBinWidth(i));
+    }
 }
 
 
-class DataSD {
-public:
-  Double_t photonPt, photonEta, photonPhi, jetPt, jetEta, jetPhi, dphi, dj, weight, zg, rg;
-};
 
+void plot() {
+    TFile *infJewelPP = new TFile("inputJetTree/jewel-pp.root");
+    TFile *infJewelPbPb = new TFile("inputJetTree/jewel-PbPb.root");
+    TFile *infPyquenPP = new TFile("inputJetTree/pyquen-pp.root");
+    TFile *infPyquenPbPb = new TFile("inputJetTree/pyquen-PbPb.root");
 
-void setBranchSD(TTree *outputTree, DataSD *data)
-{
-  outputTree->Branch("photonPt", &data->photonPt, "photonPt/D");
-  outputTree->Branch("photonEta", &data->photonEta, "photonEta/D");
-  outputTree->Branch("photonPhi", &data->photonPhi, "photonPhi/D");
-  outputTree->Branch("jetPt", &data->jetPt, "jetPt/D");
-  outputTree->Branch("jetEta", &data->jetEta, "jetEta/D");
-  outputTree->Branch("jetPhi", &data->jetPhi, "jetPhi/D");
-  outputTree->Branch("dphi", &data->dphi, "dphi/D");
-  outputTree->Branch("dj", &data->dj, "dj/D");
-  outputTree->Branch("zg", &data->zg, "zg/D");
-  outputTree->Branch("rg", &data->rg, "rg/D");
-  outputTree->Branch("weight", &data->weight, "weight/D");
-}
+    TTree *tJewelPP = (TTree*)infJewelPP->Get("JetTree");
+    TTree *tJewelPbPb = (TTree*)infJewelPbPb->Get("JetTree");
+    TTree *tPyquenPP = (TTree*)infPyquenPP->Get("JetTree");
+    TTree *tPyquenPbPb = (TTree*)infPyquenPbPb->Get("JetTree");
 
-double calcPhi(double phi)
-{
-   phi = fmod(phi + M_PI, 2*M_PI);
-   if (phi < 0) {
-       phi += 2*M_PI;
-   }
-   phi -= M_PI;
-   return phi;
-}
+    TFile *outf = new TFile("output.root", "recreate");
 
-void fillDjHist(JetTree *j, TH1D* h, Data *data, TTree *t, DataSD *dataSD, TTree *tSD, bool JEWELRecoil = true)
-{
-   double sumEntry=0;
-   h->Sumw2();
-   for (long long int i=0;i<j->fChain->GetEntries();i++) {
-      if (i%10000==0) cout <<i <<" "<<j->fChain->GetEntries()<<endl;
-      j->GetEntry(i);
-      if ((*j->LeadingPhotonPt)[0]<40) continue;
+    TTree* outputTreeJewelPP = new TTree("tJewelPP", "");
+    TTree* outputTreeJewelPbPb = new TTree("tJewelPbPb", "");
+    TTree* outputTreePyquenPP = new TTree("tPyquenPP", "");
+    TTree* outputTreePyquenPbPb = new TTree("tPyquenPbPb", "");
 
-      // R=0.3 Photon-Jet SD analysis
-      for (long unsigned int k=0;k<j->SignalJet03JewelPt->size();k++) {
-         if (((*j->SignalJet03JewelPt)[k]>30 && fabs((*j->SignalJet03JewelEta)[k])<1.6&&JEWELRecoil) ||
-	     ((*j->SignalJet03Pt)[k]>30 && fabs((*j->SignalJet03Eta)[k])<1.6&&!JEWELRecoil)
-	    ) {
-	    double dPhi_jgamma = 0;
+    EventData dataJewelPP, dataJewelPbPb, dataPyquenPP, dataPyquenPbPb;
+    setBranches(outputTreeJewelPP, dataJewelPP);
+    setBranches(outputTreeJewelPbPb, dataJewelPbPb);
+    setBranches(outputTreePyquenPP, dataPyquenPP);
+    setBranches(outputTreePyquenPbPb, dataPyquenPbPb);
 
-	    if (JEWELRecoil) dPhi_jgamma=fabs(calcPhi((*j->SignalJet03JewelPhi)[k]-(*j->LeadingPhotonPhi)[0]));
-	    else             dPhi_jgamma=fabs(calcPhi((*j->SignalJet03Phi)[k]-(*j->LeadingPhotonPhi)[0]));
+    const int nBin = 10;
+    double myBins[nBin + 1] = {0, 0.015, 0.03, 0.045, 0.06, 0.08, 0.1, 0.12, 0.15, 0.2, 0.3};
+    TH1D *hJewelPP = new TH1D("hJewelPP", "", nBin, myBins);
+    TH1D *hJewelPbPb = new TH1D("hJewelPbPb", "", nBin, myBins);
+    TH1D *hPyquenPP = new TH1D("hPyquenPP", "", nBin, myBins);
+    TH1D *hPyquenPbPb = new TH1D("hPyquenPbPb", "", nBin, myBins);
 
-	    if (dPhi_jgamma<3.14159/2.) continue;
+    JetTree *JewelPP = new JetTree(tJewelPP);
+    JetTree *JewelPbPb = new JetTree(tJewelPbPb);
+    JetTree *PyquenPP = new JetTree(tPyquenPP);
+    JetTree *PyquenPbPb = new JetTree(tPyquenPbPb);
 
-	    double dPhi = 0;
-	    if (JEWELRecoil) dPhi=calcPhi((*j->SignalJet03JewelPhi)[k]-(*j->SignalJet03WTAAxisPhi)[k]);
-	    else             dPhi=calcPhi((*j->SignalJet03Phi)[k]-(*j->SignalJet03WTAAxisPhi)[k]);
+    cout << "Processing JetTree Data" << endl;
+    processEvent(JewelPP, hJewelPP, dataJewelPP, outputTreeJewelPP);
+    processEvent(JewelPbPb, hJewelPbPb, dataJewelPbPb, outputTreeJewelPbPb);
+    processEvent(PyquenPP, hPyquenPP, dataPyquenPP, outputTreePyquenPP);
+    processEvent(PyquenPbPb, hPyquenPbPb, dataPyquenPbPb, outputTreePyquenPbPb);
 
-	    double dEta = 0;
-	    if (JEWELRecoil) dEta=(*j->SignalJet03JewelEta)[k]-(*j->SignalJet03WTAAxisEta)[k];
-	    else             dEta=(*j->SignalJet03Eta)[k]-(*j->SignalJet03WTAAxisEta)[k];
+    outf->cd();
+    hJewelPP->Write();
+    hJewelPbPb->Write();
+    hPyquenPP->Write();
+    hPyquenPbPb->Write();
+    outputTreeJewelPP->Write();
+    outputTreeJewelPbPb->Write();
+    outputTreePyquenPP->Write();
+    outputTreePyquenPbPb->Write();
 
-	    double dj = sqrt(dPhi*dPhi+dEta*dEta);
-	    h->Fill(dj,(*j->EventWeight)[0]);
-	    sumEntry+=(*j->EventWeight)[0];
-	    data->weight=(*j->EventWeight)[0];
-	    data->photonPt= (*j->LeadingPhotonPt)[0];
-	    data->photonEta= (*j->LeadingPhotonEta)[0];
-	    data->photonPhi= (*j->LeadingPhotonPhi)[0];
-	    data->jetPt= (*j->SignalJet03JewelPt)[k];
-	    data->jetEta= (*j->SignalJet03JewelEta)[k];
-	    data->jetPhi= (*j->SignalJet03JewelPhi)[k];
-	    data->dj=dj;
-	    data->dphi=dPhi_jgamma;
-	    t->Fill();
-	 }
-      }
-
-      for (long unsigned int k=0;k<j->SignalJet02JewelPt->size();k++) {
-         if ((*j->SignalJet02JewelPt)[k]>30 && fabs((*j->SignalJet02JewelEta)[k])<1.6) {
-	    double dPhi_jgamma = fabs(calcPhi((*j->SignalJet02JewelPhi)[k]-(*j->LeadingPhotonPhi)[0]));
-	    if (dPhi_jgamma<3.14159/2.) continue;
-	    double dPhi = calcPhi((*j->SignalJet02JewelPhi)[k]-(*j->SignalJet02WTAAxisPhi)[k]);
-	    double dEta = (*j->SignalJet02JewelEta)[k]-(*j->SignalJet02WTAAxisEta)[k];
-	    double dj = sqrt(dPhi*dPhi+dEta*dEta);
-	    h->Fill(dj,(*j->EventWeight)[0]);
-	    sumEntry+=(*j->EventWeight)[0];
-	    dataSD->weight=(*j->EventWeight)[0];
-	    dataSD->photonPt= (*j->LeadingPhotonPt)[0];
-	    dataSD->photonEta= (*j->LeadingPhotonEta)[0];
-	    dataSD->photonPhi= (*j->LeadingPhotonPhi)[0];
-	    dataSD->jetPt= (*j->SignalJet02JewelPt)[k];
-	    dataSD->jetEta= (*j->SignalJet02JewelEta)[k];
-	    dataSD->jetPhi= (*j->SignalJet02JewelPhi)[k];
-	    dataSD->dj=dj;
-	    dataSD->dphi=dPhi_jgamma;
-	    tSD->Fill();
-	 }
-      }
-
-   }
-   h->Scale(1./sumEntry);
-   
-   for (int i=0;i<h->GetNbinsX();i++) {
-      h->SetBinContent(i,h->GetBinContent(i)/h->GetBinWidth(i));
-      h->SetBinError(i,h->GetBinError(i)/h->GetBinWidth(i));
-   }    
-}
-
-void plot()
-{
-   TFile *infJewelPP    = new TFile("jewel-pp.root");
-   TFile *infJewelPbPb  = new TFile("jewel-PbPb.root");
-   TFile *infJewelPbPbNoRecoil  = new TFile("jewel-PbPb.root");
-   TFile *infPyquenPP   = new TFile("pyquen-pp.root");
-   TFile *infPyquenPbPb = new TFile("pyquen-PbPb.root");
-   TFile *infPyquenNoWidePbPb = new TFile("pyquenNoWide-PbPb.root");
-   
-   TTree *tJewelPP    = (TTree*)infJewelPP   ->Get("JetTree");
-   TTree *tJewelPbPb  = (TTree*)infJewelPbPb ->Get("JetTree");
-   TTree *tJewelPbPbNoRecoil  = (TTree*)infJewelPbPbNoRecoil ->Get("JetTree");
-   TTree *tPyquenPP   = (TTree*)infPyquenPP  ->Get("JetTree");
-   TTree *tPyquenPbPb = (TTree*)infPyquenPbPb->Get("JetTree");
-   TTree *tPyquenNoWidePbPb = (TTree*)infPyquenNoWidePbPb->Get("JetTree");
-   
-   TFile *outf = new TFile("output.root","recreate");
-   
-   TTree* outputTreeJewelPP = new TTree("tJewelPP","");
-   TTree* outputTreeSDJewelPP = new TTree("tSDJewelPP","");
-   Data *dataJewelPP = new Data;
-   DataSD *dataSDJewelPP = new DataSD;
-   setBranch(outputTreeJewelPP, dataJewelPP);
-   setBranchSD(outputTreeSDJewelPP, dataSDJewelPP);
-   
-   TTree* outputTreeJewelPbPb = new TTree("tJewelPbPb","");
-   TTree* outputTreeSDJewelPbPb = new TTree("tSDJewelPbPb","");
-   Data *dataJewelPbPb = new Data;
-   DataSD *dataSDJewelPbPb = new DataSD;
-   setBranch(outputTreeJewelPbPb, dataJewelPbPb);
-   setBranchSD(outputTreeSDJewelPbPb, dataSDJewelPbPb);
-      
-   TTree* outputTreeJewelPbPbNoRecoil = new TTree("tJewelPbPbNoRecoil","");
-   TTree* outputTreeSDJewelPbPbNoRecoil = new TTree("tSDJewelPbPbNoRecoil","");
-   Data *dataJewelPbPbNoRecoil = new Data;
-   DataSD *dataSDJewelPbPbNoRecoil = new DataSD;
-   setBranch(outputTreeJewelPbPbNoRecoil, dataJewelPbPbNoRecoil);
-   setBranchSD(outputTreeSDJewelPbPbNoRecoil, dataSDJewelPbPbNoRecoil);
-      
-   TTree* outputTreePyquenPP = new TTree("tPyquenPP","");
-   TTree* outputTreeSDPyquenPP = new TTree("tSDPyquenPP","");
-   Data *dataPyquenPP = new Data;
-   DataSD *dataSDPyquenPP = new DataSD;
-   setBranch(outputTreePyquenPP, dataPyquenPP);
-   setBranchSD(outputTreeSDPyquenPP, dataSDPyquenPP);
-   
-   TTree* outputTreePyquenPbPb = new TTree("tPyquenPbPb","");
-   TTree* outputTreeSDPyquenPbPb = new TTree("tSDPyquenPbPb","");
-   Data *dataPyquenPbPb = new Data;
-   DataSD *dataSDPyquenPbPb = new DataSD;
-   setBranch(outputTreePyquenPbPb, dataPyquenPbPb);
-   setBranchSD(outputTreeSDPyquenPbPb, dataSDPyquenPbPb);
-
-   TTree* outputTreePyquenNoWidePbPb = new TTree("tPyquenNoWidePbPb","");
-   TTree* outputTreeSDPyquenNoWidePbPb = new TTree("tSDPyquenNoWidePbPb","");
-   Data *dataPyquenNoWidePbPb = new Data;
-   DataSD *dataSDPyquenNoWidePbPb = new DataSD;
-   setBranch(outputTreePyquenNoWidePbPb, dataPyquenNoWidePbPb);
-   setBranchSD(outputTreeSDPyquenNoWidePbPb, dataSDPyquenNoWidePbPb);
-
-   
-   
-   const int nBin = 10;
-   double myBins[nBin+1] = {0, 0.015, 0.03, 0.045, 0.06, 0.08, 0.1, 0.12, 0.15, 0.2, 0.3};
-   
-   TH1D *hJewelPP   = new TH1D("hJewelPP","",nBin,myBins);
-   TH1D *hJewelPbPb = new TH1D("hJewelPbPb","",nBin,myBins);
-   TH1D *hJewelPbPbNoRecoil = new TH1D("hJewelPbPbNoRecoil","",nBin,myBins);
-   TH1D *hPyquenPP   = new TH1D("hPyquenPP","",nBin,myBins);
-   TH1D *hPyquenPbPb = new TH1D("hPyquenPbPb","",nBin,myBins);
-   TH1D *hPyquenNoWidePbPb = new TH1D("hPyquenNoWidePbPb","",nBin,myBins);
-   
-   
-   cout <<"JetTree"<<endl;
-   JetTree *JewelPP    = new JetTree(tJewelPP);
-   JetTree *JewelPbPb  = new JetTree(tJewelPbPb);
-   JetTree *JewelPbPbNoRecoil  = new JetTree(tJewelPbPbNoRecoil);
-   JetTree *PyquenPP   = new JetTree(tPyquenPP);
-   JetTree *PyquenPbPb = new JetTree(tPyquenPbPb);
-   JetTree *PyquenNoWidePbPb = new JetTree(tPyquenNoWidePbPb);
-
-   cout <<"Done"<<endl;
-   fillDjHist(JewelPP,hJewelPP, dataJewelPP, outputTreeJewelPP, dataSDJewelPP, outputTreeSDJewelPP);
-   fillDjHist(JewelPbPb,hJewelPbPb, dataJewelPbPb, outputTreeJewelPbPb, dataSDJewelPbPb, outputTreeSDJewelPbPb);
-   fillDjHist(JewelPbPbNoRecoil,hJewelPbPbNoRecoil, dataJewelPbPbNoRecoil, outputTreeJewelPbPbNoRecoil, dataSDJewelPbPbNoRecoil, outputTreeSDJewelPbPbNoRecoil,0);
-   fillDjHist(PyquenPP,hPyquenPP, dataPyquenPP, outputTreePyquenPP, dataSDPyquenPP, outputTreeSDPyquenPP);
-   fillDjHist(PyquenPbPb,hPyquenPbPb, dataPyquenPbPb, outputTreePyquenPbPb, dataSDPyquenPbPb, outputTreeSDPyquenPbPb);
-   fillDjHist(PyquenNoWidePbPb,hPyquenNoWidePbPb, dataPyquenNoWidePbPb, outputTreePyquenNoWidePbPb, dataSDPyquenNoWidePbPb, outputTreeSDPyquenNoWidePbPb);
-   
-   hJewelPP->Draw();
-   hJewelPP->Write();
-   hJewelPbPb->Write();
-   hJewelPbPbNoRecoil->Write();
-   hPyquenPP->Write();
-   hPyquenPbPb->Write();
-   hPyquenNoWidePbPb->Write();
-   outputTreeJewelPP->Write();
-   outputTreeJewelPbPb->Write();
-   outputTreeJewelPbPbNoRecoil->Write();
-   outputTreePyquenPP->Write();
-   outputTreePyquenPbPb->Write();
-   outputTreePyquenNoWidePbPb->Write();
-   outputTreeSDJewelPP->Write();
-   outputTreeSDJewelPbPb->Write();
-   outputTreeSDPyquenPP->Write();
-   outputTreeSDPyquenPbPb->Write();
-   outputTreeSDPyquenNoWidePbPb->Write();
-   
+    outf->Close();
+    cout << "Done" << endl;
 }
